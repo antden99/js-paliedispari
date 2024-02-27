5418dbe216374abbadba01a5da71101ab722cd41
antden99 <antoniodenigris@icloud.com>
2024-02-27 15:35:27 +0100

    add changes in function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,7 @@
 
 const userWord = prompt("inserisci una parola");
 console.log(`la parola che hai inserito è: ${userWord}`);
-let value = false;
-const list = [];
+
 
 /*creo un ciclo for che mi scorra la stringa inserita al contrario e la salvi in un array vuoto
 for(i=userWord.length-1; i>=0;i--){
@@ -30,22 +29,23 @@ if(value){
 
 //creo la funzione che controlla se palindroma
 
-function ceckIfPalindrome(userWord){
+function ceckIfPalindrome(word) {
+    let value = false;
+    const list = [];
 
-    
-    for(i=userWord.length-1; i>=0;i--){
-        const caract = userWord[i];
+    for (i = word.length - 1; i >= 0; i--) {
+        const caract = word[i];
         //console.log(caract);
         list.push(caract);
     }
 
-    for (let i = 0; i < userWord.length; i++) {
-        if (list[i] != userWord[i]){
+    for (let i = 0; i < word.length; i++) {
+        if (list[i] != word[i]) {
             value = true;
-        } 
+        }
     }
 
-    if(value){
+    if (value) {
         console.log("la parola inserita non è palindroma");
     } else {
         console.log("la parola inserita è palindroma");
